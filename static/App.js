db77d11720bd4be8166b49d641e58c058c75dd6a
Thien Tin <dttin94@gmail.com>
2017-06-17 08:42:05 +0700

    Using POST, GET and implement validate request

--- static/App.js
+++ static/App.js
@@ -189,16 +189,44 @@ var IssueAdd = function (_React$Component4) {
   function IssueAdd() {
     _classCallCheck(this, IssueAdd);
 
-    return _possibleConstructorReturn(this, (IssueAdd.__proto__ || Object.getPrototypeOf(IssueAdd)).apply(this, arguments));
+    var _this4 = _possibleConstructorReturn(this, (IssueAdd.__proto__ || Object.getPrototypeOf(IssueAdd)).call(this));
+
+    _this4.handleSubmit = _this4.handleSubmit.bind(_this4);
+    return _this4;
   }
 
   _createClass(IssueAdd, [{
+    key: 'handleSubmit',
+    value: function handleSubmit(e) {
+      e.preventDefault();
+      var form = document.forms.issueAdd;
+      this.props.createIssue({
+        owner: form.owner.value,
+        title: form.title.value,
+        status: 'New',
+        created: new Date()
+      });
+      // clear the form for the next input
+      form.owner.value = '';
+      form.title.value = '';
+    }
+  }, {
     key: 'render',
     value: function render() {
       return React.createElement(
         'div',
         null,
-        'This is a placeholder for an Issue Add entry form.'
+        React.createElement(
+          'form',
+          { name: 'issueAdd', onSubmit: this.handleSubmit },
+          React.createElement('input', { type: 'text', name: 'owner', placeholder: 'Owner' }),
+          React.createElement('input', { type: 'text', name: 'title', placeholder: 'Title' }),
+          React.createElement(
+            'button',
+            null,
+            'Add'
+          )
+        )
       );
     }
   }]);
@@ -216,8 +244,7 @@ var IssueList = function (_React$Component5) {
 
     _this5.state = { issues: [] };
 
-    _this5.createTestIssue = _this5.createTestIssue.bind(_this5);
-    setTimeout(_this5.createTestIssue.bind(_this5), 2000);
+    _this5.createIssue = _this5.createIssue.bind(_this5);
     return _this5;
   }
 
@@ -244,16 +271,6 @@ var IssueList = function (_React$Component5) {
       this.setState({ issues: newIssues });
     }
   }, {
-    key: 'createTestIssue',
-    value: function createTestIssue() {
-      this.createIssue({
-        status: 'New',
-        owner: 'Pieta',
-        created: new Date(),
-        title: 'Completion date should be optional'
-      });
-    }
-  }, {
     key: 'render',
     value: function render() {
       return React.createElement(
@@ -267,13 +284,8 @@ var IssueList = function (_React$Component5) {
         React.createElement(IssueFilter, null),
         React.createElement('hr', null),
         React.createElement(IssueTable, { issues: this.state.issues }),
-        React.createElement(
-          'button',
-          { onClick: this.createTestIssue },
-          'Add'
-        ),
         React.createElement('hr', null),
-        React.createElement(IssueAdd, null)
+        React.createElement(IssueAdd, { createIssue: this.createIssue })
       );
     }
   }]);
